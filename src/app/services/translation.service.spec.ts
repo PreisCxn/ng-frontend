ca7a1bf1f17463dfcf9fa0d63edb5c3dfc828f07
Alive <nicolasfr002@gmail.com>
2023-12-17 15:39:44 +0100

    feat: Implement TranslationService with multi-language support
    - Implement TranslationService to support multiple languages
    - Add functions to set and get the current language and to retrieve translations
    - Add tests for TranslationService, including tests for changing the current language and retrieving translations
    - Update tsconfig file to enable loading of JSON modules
    - Add language files for German, English, and MemeCxn

--- src/app/services/translation.service.spec.ts
+++ src/app/services/translation.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TranslationService } from './translation.service';
+import {Languages} from "./languages";
 
 describe('TranslationService', () => {
   let service: TranslationService;
@@ -13,4 +14,22 @@ describe('TranslationService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should change current language', () => {
+    service.setLanguage(Languages.German);
+    expect(service.getCurrentLanguage()).toEqual(Languages.German);
+    service.setLanguage(Languages.English);
+    expect(service.getCurrentLanguage()).toEqual(Languages.English);
+  });
+
+  it('should return translation', async () => {
+    await service.setLanguage(Languages.German);
+    console.log(service.getTranslation('testing.lang'));
+    expect(service.getTranslation('testing.lang')).toEqual(Languages.German);
+    await service.setLanguage(Languages.English);
+    expect(service.getTranslation('testing.lang')).toEqual(Languages.English);
+    await service.setLanguage(Languages.MemeCxn);
+    expect(service.getTranslation('testing.lang')).toEqual(Languages.MemeCxn);
+  });
+
 });
