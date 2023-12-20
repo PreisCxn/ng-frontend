f07c6d9a9165a9e975299926df4089de8c5bd4ee
Alive <nicolasfr002@gmail.com>
2023-12-20 02:08:59 +0100

    .

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HeaderService} from "../services/header.service";
+import {CommonModule} from "@angular/common";
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  darkMode:boolean = true;
+
   constructor(public headerService: HeaderService) {
+  }
+
+  toggleMode(): void {
+    this.darkMode = !this.darkMode;
   }
 
 }
