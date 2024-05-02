b2aa72dfbee80c11a2b53c9df3a9e0cb1141bea7
Alive <nicolasfr002@gmail.com>
2024-05-02 16:04:36 +0200

    [PCXN-26] added component for mod site

---
+++ src/app/mc-mod/mc-mod.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mc-mod',
+  standalone: true,
+  imports: [],
+  templateUrl: './mc-mod.component.html',
+  styleUrl: './mc-mod.component.scss'
+})
+export class McModComponent {
+
+}
