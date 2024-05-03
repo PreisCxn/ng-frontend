44a084902aeb18e13dbd4ff8adde0f9a1310721f
Alive <nicolasfr002@gmail.com>
2024-05-03 14:08:56 +0200

    [PCXN-26] added heading service init to mod side

--- src/app/mc-mod/mc-mod.component.ts
+++ src/app/mc-mod/mc-mod.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HeaderService, MenuActives} from "../shared/header.service";
+import {RedirectService} from "../shared/redirect.service";
 
 @Component({
   selector: 'app-mc-mod',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './mc-mod.component.html',
   styleUrl: './mc-mod.component.scss'
 })
-export class McModComponent {
+export class McModComponent implements OnInit {
+
+  constructor(private headerService: HeaderService, protected redirect: RedirectService) {
+    this.headerService.init(
+      "pcxn.subsite.mod.sectionTitle",
+      false,
+      false,
+      MenuActives.MOD
+    );
+  }
+
+  ngOnInit(): void {
+    this.redirect.resetQueryParams();
+    this.redirect.scrollToTop(false);
+  }
 
 }
